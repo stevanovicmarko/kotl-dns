33fe0fa34afdf37f4bbaa3fb3842ae165ee5a480
Marko Stevanović <stevanovicmarko@gmail.com>
2023-06-12 21:22:51 +0200

    Optimize resolve function.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,29 +1,26 @@
 import dns.DnsQuery
 import dns.DnsRecordType
 
-const val DEFAULT_NAMSESERVER = "198.41.0.4"
+const val DEFAULT_NAMESERVER = "198.41.0.4"
 fun resolve(domainName: String, recordType: DnsRecordType): String {
-    var nameserver = DEFAULT_NAMSESERVER
+    var nameserver = DEFAULT_NAMESERVER
     while (true) {
         val query = DnsQuery(domainName, recordType)
         val response = query.sendQuery(nameserver)
 
-        val ip = query.getAnswer(response)
-        nameserver = if (ip != null) {
-            return ip
-        } else if (query.getNameServerIp(response) != null) {
-            query.getNameServerIp(response)!!
-        } else if (query.getNameServer(response) != null) {
-            resolve(query.getNameServer(response)!!, DnsRecordType.A)
-        } else {
-            throw Exception("No answer found")
+        query.getAnswer(response)?.let {
+            return it
         }
+
+        nameserver = query.getNameServerIp(response)
+            ?: query.getNameServer(response)?.let { resolve(it, DnsRecordType.A) }
+                    ?: throw Exception("No answer found")
     }
 }
 
 
 fun main(args: Array<String>) {
 
-    val ipAddress = resolve("google.com", DnsRecordType.A)
+    val ipAddress = resolve("twitter.com", DnsRecordType.A)
     println(ipAddress)
 }
